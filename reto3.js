359102858f50cf97514630c72174dfe2ab380455
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-01-10 14:55:40 +0100

    reto3

--- reto3.js
+++ reto3.js
@@ -4,19 +4,25 @@ const readline = require ('readline');
 
 const rl = readline.createInterface(process.stdin, process.stdout);
 
+let person = {
+    name: '',
+    surName: '',
+    age:0
+}; 
+
 
 rl.question('Cuál es tu nombre?', (name)=>{
+    person.name = name; 
     rl.question('Cuál es tu apellido?', (surName)=>{
+        person.surName = surName; 
          rl.question('Cuál es tu edad?', (age)=>{
-            console.log('Tu nombre es:' + name + '\n' 
-            + 'Tu apellido es:' + surName + '\n' 
-            + 'Tu edad es:' + age);
+            person.age = age; 
+            let personJson = JSON.stringify(person); 
+            console.log(personJson);
             rl.close(); 
         });
     });
 });
-
-
 
 
 
