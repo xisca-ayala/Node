a8d67e5af9ed6deb933eddbe2ba16cf3a0288769
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-01-12 10:51:52 +0100

    cambios

--- fs.js
+++ fs.js
@@ -15,13 +15,14 @@ fs.writeFile('data.json', userJson, (err)=> {
         console.error(err);
     }else{
         console.log ('El arxivo ha sido escrito con éxito')
+        fs.readFile('data.json', 'utf-8', (err,data)=> {
+            if(err){
+                console.error(err);
+            }
+            console.log(JSON.parse(data));
+    })
     }
 });
 
-fs.readFile('data.json', 'utf-8', (err,data)=> {
-    if(err){
-        console.error(err);
-    }
-    console.log(JSON.parse(data));
-})
+
 
